ea45fb92ae89df7a704b72010f4560c94774bbad
prabpypipo <46016382+prabpypipo@users.noreply.github.com>
2021-09-28 23:29:38 +0800

    finished

--- lab2/exercise/exercise.c
+++ lab2/exercise/exercise.c
@@ -18,24 +18,20 @@ TIP: You can use sscanf to convert char array to int and sprintf to convert int 
 
 */
 int main(){
-    FILE* fd ;
-    FILE* fd_1;
+    int fd_1 , fd_2 ;
     char str[1000] ;
 
-    fd = fopen("/input.txt" , "r") ;
-    fd_1 = fopen("/output.txt" , "w+") ;
+    fd_1 = open("input.txt", O_RDONLY, S_IRUSR | S_IWUSR ) ;
+    fd_2 = open("output.txt", O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR ) ;
 
+    read(fd_1, str, 100) ;
+    int a[15] ;
 
-    int i ;
-    for(i = 0 ; i < 10 ; i ++){
-        fscanf(fd, "%s", str) ;
-        int num ;
-        sscanf(str, "%d", &num) ;
-        num ++ ;
-        sprintf(str, "%d" , num) ;
+    sscanf(str, "%d %d %d %d %d %d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]) ;
+    sprintf(str, "%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d", a[0] + 1, a[1] + 1, a[2] + 1, a[3] + 1, a[4] + 1, a[5] + 1, a[6] + 1, a[7] + 1, a[8] + 1, a[9] + 1) ;
 
-        fprintf(fd_1, "%s", str) ;
-        fprintf(fd_1, "\n") ;
+    write(fd_2, str, strlen(str)) ;
 
-    }
+    close(fd_1) ;
+    close(fd_2) ;
 }
